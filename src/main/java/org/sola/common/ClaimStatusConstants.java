75082476821247e075e938bf3646fa7d46cdc4a1
Alexander Solovov <a_solovov@yahoo.com>
2017-06-16 14:45:13 +0600

    Initial commit

---
+++ src/main/java/org/sola/common/ClaimStatusConstants.java
@@ -0,0 +1,28 @@
+package org.sola.common;
+
+public class ClaimStatusConstants {
+    
+    /** Withdrawn status of claim. */
+    public static final String WITHDRAWN = "withdrawn";
+    
+    /** Created status of claim. */
+    public static final String CREATED = "created";
+    
+    /** Rejected status of claim. */
+    public static final String REJECTED = "rejected";
+    
+    /** Reviewed status of claim. */
+    public static final String REVIEWED = "reviewed";
+    
+    /** Moderated status of claim. */
+    public static final String MODERATED = "moderated";
+    
+    /** Unmoderated status of claim. */
+    public static final String UNMODERATED = "unmoderated";
+    
+    /** Issued status of claim. */
+    public static final String ISSUED = "issued";
+    
+    /** Historic status of claim. */
+    public static final String HISTORIC = "historic";
+}
